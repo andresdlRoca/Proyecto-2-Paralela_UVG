2bdd08d325e0c948dda9988e1320945ea828d380
Diego-CB <cordova15441@gmail.com>
2023-10-12 17:48:39 -0600

    more code Cleaning

--- README.md
+++ README.md
@@ -11,15 +11,41 @@
 
 ### Uso
 
-## Programa Paralelo
+## Programa Paralelo con BruteForce
 
 ### Prerequisitos
+- Maquina Virtual (o fisica) con linux
+- Compilador gcc
 
 ### Dependencias
+- libreria Open MPI
+- Libreria Open SSL
+
+> Instalacion de Open SSL:
+> 1. `sudo apt update`
+> 2. `sudo apt upgrade`
+> 3. `sudo apt install libssl-dev`
 
 ### Compilacion
 
+`mpicc -o bruteforce bruteforce.c -lcrypto -lssl`
+
 ### Uso
+
+`mpirun -np 4 bruteforce`
+
+> Nota: Se debe especificar en el archivo `./input.txt`:
+> 1. La llave privada para encriptar
+> 2. El mensaje para encriptar
+> 3. La *llave publica* (mensaje a buscar para verificar desincriptacion)
+>
+> Ejemplo:
+>> 123456
+>>
+>> Esta es una prueba de proyecto 2
+>>
+>> es una prueba de
+>>
 
 
 ## Autores
--- bf_txt.c
+++ bf_txt.c
@@ -1,6 +1,6 @@
 //bruteforce.c
 //nota: el key usado es bastante pequenio, cuando sea random speedup variara
-// compilar: mpicc -o bruteforce bruteforce.c -lcrypto
+// compilar: mpicc -o bruteforce bruteforce.c -lcrypto -lssl
 // ejecutar: mpirun -np 4 bruteforce
 
 #include <string.h>
@@ -51,119 +51,119 @@ void encrypt(long key, char *ciph, int len) {
 }
 
 int tryKey(long key, char *ciph, int len, char search[]){
-  char temp[len+1];
-  memcpy(temp, ciph, len);
-  temp[len]=0;
-  decrypt(key, temp, len);
-  return strstr((char *)temp, search) != NULL;
+    char temp[len+1];
+    memcpy(temp, ciph, len);
+    temp[len]=0;
+    decrypt(key, temp, len);
+    return strstr((char *)temp, search) != NULL;
 }
 
 int main(int argc, char *argv[]){ //char **argv
-  // MPi Init
-  int N, id;
-  long upper = (1L <<56); //upper bound DES keys 2^56
-  long mylower, myupper;
-  MPI_Status st;
-  MPI_Request req;
-  int flag;
-  char keyLine[64];
-  unsigned char cipherLine[64];
-  MPI_Comm comm = MPI_COMM_WORLD;
+    // MPi Init
+    int N, id;
+    long upper = (1L <<56); //upper bound DES keys 2^56
+    long mylower, myupper;
+    MPI_Status st;
+    MPI_Request req;
+    int flag;
+    char keyLine[64];
+    unsigned char cipherLine[64];
+    MPI_Comm comm = MPI_COMM_WORLD;
 
-  MPI_Init(NULL, NULL);
-  MPI_Comm_size(comm, &N);
-  MPI_Comm_rank(comm, &id);
-  double tstart, tend;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(comm, &N);
+    MPI_Comm_rank(comm, &id);
+    double tstart, tend;
 
 
-  // Lectura de txt
-  char inputFileName[] = "input.txt"; // Nombre del archivo de entrada
-  char search[64]; // Cadena de búsqueda
+    // Lectura de txt
+    char inputFileName[] = "input.txt"; // Nombre del archivo de entrada
+    char search[64]; // Cadena de búsqueda
 
-  // Abrir el archivo de entrada
-  FILE *fileIn = fopen(inputFileName, "r");
-  if (fileIn == NULL) {
-    perror("Error al abrir el archivo de entrada");
-    return 1;
-  }
-
-  // Leer la clave
-  if (fgets(keyLine, sizeof(keyLine), fileIn) == NULL) {
-    perror("Error al leer la clave del archivo");
-    fclose(fileIn);
-    return 1;
-  }
-
-  long key = atol(keyLine); // Convertir la clave a long
-  if (id == 0){
-    printf("------ Encriptacion ------\n");
-    printf("Llave privada a utilizar: %ld\n", key);
-  }
-
-  // Leer el texto cifrado
-  if (fgets(cipherLine, sizeof(cipherLine), fileIn) == NULL) {
-    perror("Error al leer el texto cifrado del archivo");
-    fclose(fileIn);
-    return 1;
-  }
-
-  // Leer cadena de busqueda
-  if (fgets(search, sizeof(search), fileIn) == NULL) {
-    perror("Error al leer el texto cifrado del archivo");
-    fclose(fileIn);
-    return 1;
-  }
-
-  if (id == 0){
-    printf("Mensaje Original: %s\n", cipherLine);
-  }
-
-  // Cifrar texto
-  char buffer[512];
-  encrypt(key, cipherLine, sizeof(cipherLine));
-  fclose(fileIn);
-
-  if (id == 0){
-    printf("Mensaje Cifrado: %s\n", cipherLine);
-    printf("\n------ Desencriptando por Bruteforce ------\n");
-  }
-
-  MPI_Barrier(MPI_COMM_WORLD); // Barrera MPI
-  int ciphlen = strlen(cipherLine);
-  if(id==0){
-    tstart = MPI_Wtime();
-  }
-
-  // Descifrado
-  int range_per_node = upper / N;
-  mylower = range_per_node * id;
-  myupper = range_per_node * (id+1) -1;
-  if(id == N-1){
-    //compensar residuo
-    myupper = upper;
-  }
-
-  long found = 0;
-
-  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
-
-  for(int i = mylower; i<myupper && (found==0); ++i){
-    if(tryKey(i, (char *)cipherLine, ciphlen, search)){
-      found = i;
-      for(int node=0; node<N; node++){
-        MPI_Send(&found, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
-      }
-      break;
+    // Abrir el archivo de entrada
+    FILE *fileIn = fopen(inputFileName, "r");
+    if (fileIn == NULL) {
+        perror("Error al abrir el archivo de entrada");
+        return 1;
     }
-  }
 
-  if(id==0){
-    tend = MPI_Wtime();
-    MPI_Wait(&req, &st);
-    decrypt(found, (char *)cipherLine, ciphlen);
-    printf("%li %s\n", found, cipherLine);
-    printf("\nTook %f ms to run\n", (tend-tstart) * 1000);
-  }
+    // Leer la clave
+    if (fgets(keyLine, sizeof(keyLine), fileIn) == NULL) {
+        perror("Error al leer la clave del archivo");
+        fclose(fileIn);
+        return 1;
+    }
 
-  MPI_Finalize();
+    long key = atol(keyLine); // Convertir la clave a long
+    if (id == 0){
+        printf("------ Encriptacion ------\n");
+        printf("Llave privada a utilizar: %ld\n", key);
+    }
+
+    // Leer el texto cifrado
+    if (fgets(cipherLine, sizeof(cipherLine), fileIn) == NULL) {
+        perror("Error al leer el texto cifrado del archivo");
+        fclose(fileIn);
+        return 1;
+    }
+
+    // Leer cadena de busqueda
+    if (fgets(search, sizeof(search), fileIn) == NULL) {
+        perror("Error al leer el texto cifrado del archivo");
+        fclose(fileIn);
+        return 1;
+    }
+
+    if (id == 0){
+        printf("Mensaje Original: %s", cipherLine);
+    }
+
+    // Cifrar texto
+    char buffer[512];
+    encrypt(key, cipherLine, sizeof(cipherLine));
+    fclose(fileIn);
+
+    if (id == 0){
+        printf("Mensaje Cifrado: %s\n", cipherLine);
+        printf("\n------ Desencriptando por Bruteforce ------\n");
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD); // Barrera MPI
+    int ciphlen = strlen(cipherLine);
+    if(id==0){
+        tstart = MPI_Wtime();
+    }
+
+    // Descifrado
+    int range_per_node = upper / N;
+    mylower = range_per_node * id;
+    myupper = range_per_node * (id+1) -1;
+    if(id == N-1){
+        //compensar residuo
+        myupper = upper;
+    }
+
+    long found = 0;
+
+    MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+
+    for(int i = mylower; i<myupper && (found==0); ++i){
+        if(tryKey(i, (char *)cipherLine, ciphlen, search)){
+        found = i;
+        for(int node=0; node<N; node++){
+            MPI_Send(&found, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
+        }
+        break;
+        }
+    }
+
+    if(id==0){
+        tend = MPI_Wtime();
+        MPI_Wait(&req, &st);
+        decrypt(found, (char *)cipherLine, ciphlen);
+        printf("%li %s\n", found, cipherLine);
+        printf("Took %f ms to run\n", (tend-tstart) * 1000);
+    }
+
+    MPI_Finalize();
 }
