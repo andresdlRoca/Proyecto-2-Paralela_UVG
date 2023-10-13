20e223fbc0d88f27c98855b0d2d5eb1b7cdfba51
andresdlroca <andresdelaroca11@gmail.com>
2023-10-13 13:47:23 -0600

    Fix programaa seecuencial

--- secuencial.c
+++ secuencial.c
@@ -1,5 +1,3 @@
-// Compilar: gcc -o secuencial secuencial.c
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,50 +23,59 @@ int main() {
     char ciphertext[50]; // Tamaño suficiente para almacenar el texto cifrado
     char key[50];       // Tamaño suficiente para almacenar la clave
 
+    // Caracteres alfanuméricos permitidos
+    char alphanum[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
     // Inicializar el generador de números aleatorios con una semilla
-    srand(time(NULL));
 
-    for (int keyLen = 1; keyLen <= 6; keyLen++) {
-        // Generar una clave aleatoria
-        for (int i = 0; i < keyLen; i++) {
-            key[i] = 'A' + rand() % 26; // Clave compuesta por letras mayúsculas random
-        }
-        key[keyLen] = '\0';
+    printf("Ingrese la key: ");
+    scanf("%s", key);
 
-        // Copiar el texto original en la variable ciphertext
-        strcpy(ciphertext, text);
+    // Obtener la longitud de la clave
+    int keyLen = strlen(key);
 
-        // Cifrar el texto con la clave generada
-        encrypt(ciphertext, key);
+    key[keyLen] = '\0';
 
-        // Aquí puedes medir el tiempo y realizar la búsqueda de la clave
-        clock_t start_time = clock();
+    // Copiar el texto original en la variable ciphertext
+    strcpy(ciphertext, text);
 
-        // Implementa la búsqueda de fuerza bruta aquí
-        char foundKey[50]; // Almacenará la clave encontrada
-        for (int i = 0; i < keyLen; i++) {
-            foundKey[i] = 'A';
-        }
-        foundKey[keyLen] = '\0';
+    // Cifrar el texto con la clave generada
+    encrypt(ciphertext, key);
 
-        while (strcmp(foundKey, key) != 0) {
-            // Generar la siguiente clave candidata
-            for (int i = keyLen - 1; i >= 0; i--) {
-                if (foundKey[i] < 'Z') {
-                    foundKey[i]++;
-                    break;
-                } else {
-                    foundKey[i] = 'A';
-                }
+    // Aquí puedes medir el tiempo y realizar la búsqueda de la clave
+    clock_t start_time = clock();
+
+    // Implementa la búsqueda de fuerza bruta aquí
+    char foundKey[50]; // Almacenará la clave encontrada
+    for (int i = 0; i < keyLen; i++) {
+        foundKey[i] = 'A';
+    }
+    foundKey[keyLen] = '\0';
+
+    while (strcmp(foundKey, key) != 0) {
+        // Generar la siguiente clave candidata
+        for (int i = keyLen - 1; i >= 0; i--) {
+            int index = strchr(alphanum, foundKey[i]) - alphanum;
+            if (index < strlen(alphanum) - 1) {
+
+                foundKey[i] = alphanum[index + 1];
+            } else {
+                foundKey[i] = alphanum[0];
+            }
+            if (foundKey[i] == 'A') {
+                continue; // Carryover to the next character
+            } else {
+                break;
             }
         }
-
-        clock_t end_time = clock();
-        double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-        // Imprimir resultados
-        printf("Key Length: %d, Found Key: %s, Elapsed Time: %f seconds\n", keyLen, foundKey, elapsed_time);
     }
+
+    clock_t end_time = clock();
+    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    // Imprimir resultados
+    printf("Key Length: %d, Found Key: %s, Elapsed Time: %f seconds\n", keyLen, foundKey, elapsed_time);
+
 
     return 0;
 }
