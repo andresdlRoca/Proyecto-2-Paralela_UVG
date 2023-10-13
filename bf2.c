d5bf2ea8ed1565a57610e17543576ee9f0a49b97
Paola Contreras <paolacontreras231@gmail.com>
2023-10-12 21:13:22 -0600

    2nd algorithm

---
+++ bf2.c
@@ -0,0 +1,188 @@
+//bruteforce.c
+//nota: el key usado es bastante pequenio, cuando sea random speedup variara
+// compilar: mpicc -o bruteforce bruteforce.c -lcrypto
+// ejecutar: mpirun -np 4 bruteforce
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <unistd.h>
+#include <openssl/des.h>
+
+void decrypt(long key, char *ciph, int len) {
+    DES_key_schedule keysched;
+    DES_cblock des_key;
+
+    // Set parity of key
+    for (int i = 0; i < 8; ++i) {
+        key <<= 1;
+        des_key[i] = (key & (0xFE << i * 8)) >> (i * 8);
+    }
+
+    // Initialize the key schedule
+    DES_set_odd_parity(&des_key);
+    DES_set_key_checked(&des_key, &keysched);
+
+    // Decrypt the data
+    for (int i = 0; i < len; i += 8) {
+        DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &keysched, DES_DECRYPT);
+    }
+}
+
+void encrypt(long key, char *ciph, int len) {
+    DES_key_schedule keysched;
+    DES_cblock des_key;
+
+    // Set parity of key
+    for (int i = 0; i < 8; ++i) {
+        key <<= 1;
+        des_key[i] = (key & (0xFE << i * 8)) >> (i * 8);
+    }
+
+    // Initialize the key schedule
+    DES_set_odd_parity(&des_key);
+    DES_set_key_checked(&des_key, &keysched);
+
+    // Encrypt the data
+    for (int i = 0; i < len; i += 8) {
+        DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &keysched, DES_ENCRYPT);
+    }
+}
+
+int tryKey(long key, char *ciph, int len,char search[]){
+  char temp[len+1];
+  memcpy(temp, ciph, len);
+  temp[len]=0;
+  decrypt(key, temp, len);
+  return strstr((char *)temp, search) != NULL;
+}
+
+int tryKeyRange(long start, long end, char *ciph, int len, char search[]) {
+    // Función que prueba un rango de claves
+    for (long key = start; key <= end; key++) {
+        if (tryKey(key, ciph, len, search)) {
+            return key;  // Si se encuentra la clave, devolverla
+        }
+    }
+    return -1;  // Devolver -1 si no se encuentra la clave en el rango
+}
+
+int main(int argc, char *argv[]){ //char **argv
+  int flag;
+  int N, id;
+  long mylower, myupper;
+  long upper = (1L <<56); //upper bound DES keys 2^56
+ 
+  MPI_Status st;
+  MPI_Request req;
+
+ 
+  char keyLine[64];
+  unsigned char cipherLine[64];
+  int ciphlen = strlen(cipherLine);
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(comm, &N);
+  MPI_Comm_rank(comm, &id);
+  double tstart, tend;
+
+
+  // Lectura de txt
+  char inputFileName[] = "input.txt"; // Nombre del archivo de entrada
+  char search[64]; // Cadena de búsqueda
+
+  // Abrir el archivo de entrada
+  FILE *fileIn = fopen(inputFileName, "r");
+  if (fileIn == NULL) {
+    perror("Error al abrir el archivo de entrada");
+    MPI_Finalize();
+    return 1;
+  }
+  
+  // Leer la clave
+  if (fgets(keyLine, sizeof(keyLine), fileIn) == NULL) {
+    perror("Error al leer la clave del archivo");
+    fclose(fileIn);
+    return 1;
+  }
+
+  long key = atol(keyLine); // Convertir la clave a long
+  if (id == 0){
+    printf("------ Encriptacion ------\n");
+    printf("Llave privada a utilizar: %ld\n", key);
+  }
+
+ // Leer el texto cifrado
+  if (fgets(cipherLine, sizeof(cipherLine), fileIn) == NULL) {
+    perror("Error al leer el texto cifrado del archivo");
+    fclose(fileIn);
+    return 1;
+  }
+
+  // Leer cadena de busqueda
+  if (fgets(search, sizeof(search), fileIn) == NULL) {
+    perror("Error al leer el texto cifrado del archivo");
+    fclose(fileIn);
+    return 1;
+  }
+
+  if (id == 0){
+    printf("Mensaje Original: %s\n", cipherLine);
+  }
+
+  // Cifrar texto
+  char buffer[512];
+  encrypt(key, cipherLine, sizeof(cipherLine));
+  fclose(fileIn);
+
+ if (id == 0){
+    printf("Mensaje Cifrado: %s\n", cipherLine);
+    printf("------ Desencriptando por Bruteforce ------\n");
+  }
+ 
+
+
+
+
+  // Descifrado
+// Calcula el rango de claves para este proceso
+    long range_per_node = upper / N;
+    mylower = range_per_node * id;
+    myupper = range_per_node * (id + 1) - 1;
+
+    if (id == N - 1) {
+        // Compensar residuo
+        myupper = upper;
+    }
+
+    // Variables para almacenar la clave encontrada
+    long foundKey = -1;
+
+    // Ejecuta la búsqueda de clave en el rango asignado
+    foundKey = tryKeyRange(mylower, myupper, cipherLine, ciphlen, search);
+
+    // Realiza una reducción MPI para encontrar la clave en todos los procesos
+    MPI_Allreduce(&foundKey, &foundKey, 1, MPI_LONG, MPI_MAX, comm);
+
+    if (foundKey != -1) {
+        // Un proceso ha encontrado la clave
+        if (id == 0) {
+            tend = MPI_Wtime();
+            // El proceso 0 muestra el resultado
+            decrypt(foundKey, cipherLine, ciphlen);
+            printf("Clave encontrada: %ld\n", foundKey);
+            printf("Mensaje descifrado: %s\n", (char *)cipherLine);
+            printf("\nTook %f ms to run\n", (tend-tstart) * 1000);
+        }
+    } else {
+        if (id == 0) {
+            // Ningún proceso encontró la clave
+            printf("La clave no fue encontrada en el rango de claves.\n");
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
